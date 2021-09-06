1719840dfdcbae1f5143f277969a2c3f9ec5d174
FranthescoAraujo <89648233+FranthescoAraujo@users.noreply.github.com>
2021-09-06 20:33:36 +0000

    Terceiro Commit

--- NaturalLanguageProcessing/tensorflowTeste.py
+++ NaturalLanguageProcessing/tensorflowTeste.py
@@ -1,3 +1,27 @@
 import tensorflow as tf
 
-print(tf.__version__)
+# with tf.compat.v1.Session() as sess:
+#     frase = tf.constant('Olá Mundo!')
+#     rodar = sess.run(frase)
+#     print(rodar.decode('UTF-8'))
+
+# frase = tf.constant("Olá Mundo!")
+# print(frase)
+
+a = tf.constant(5)
+b = tf.constant(3)
+c = tf.constant(2)
+
+d = tf.multiply(a, b)
+e = tf.add(b, c)
+f = tf.subtract(d,e)
+
+saida = tf.get_static_value(a)
+tf.print(b)
+tf.print(c)
+tf.print(d)
+tf.print(e)
+tf.print(f)
+
+print("###################")
+print(f)
