93713c3b3b9492c94c3bc2e4a0b9adc56f09f7cd
FranthescoAraujo <89648233+FranthescoAraujo@users.noreply.github.com>
2021-09-09 00:19:29 +0000

    08-09-2021

--- NaturalLanguageProcessing/NLP.py
+++ NaturalLanguageProcessing/NLP.py
@@ -1,18 +1,27 @@
+from gensim import models
 from PreProcessing import PreProcessing
 from DocumentRepresentationDoc2Vec import DocumentRepresentationDoc2Vec
 from TermFrequencyInverseDocumentFrequency import TermFrequencyInverseDocumentFrequency
 from DocumentRepresentationWord2Vec import DocumentRepresentationWord2Vec
 import os
 
-CORPUS_PATH = "../Corpus/"
+import tensorflow.keras as keras
+import numpy as np
 
-files = os.listdir(CORPUS_PATH + "Fake/")
+#CORPUS_PATH = "../Corpus/"
+CORPUS_PATH = "../Teste/"
 
+folders = os.listdir(CORPUS_PATH)
 listNews = []
-
-for file in files:
-    f = open(CORPUS_PATH + "Fake/" + file, "r")
-    listNews.append(f.read())
+listLabel = []
+for folder in folders:
+    for file in os.listdir(CORPUS_PATH + folder):
+        f = open(CORPUS_PATH + folder + "/" + file, "r")
+        listNews.append(f.read())
+        if folder == "Fake":
+            listLabel.append(1)
+            continue
+        listLabel.append(0)
 
 newlistNews = PreProcessing.removeAccentuation(listNews)
 newlistNews = PreProcessing.removeSpecialCharacters(newlistNews)
@@ -21,15 +30,16 @@ newlistNews = PreProcessing.toLowerCase(newlistNews)
 
 doc2vec = DocumentRepresentationDoc2Vec(newlistNews)
 listDoc2vecDM = doc2vec.paragraphVectorDistributedMemory()
-listDoc2vecDBOW = doc2vec.paragraphVectorDistributedBagOfWords()
-listDoc2vecConcat = doc2vec.concatBothParagraphVectors()
 
-word2vec = DocumentRepresentationWord2Vec(newlistNews)
-listWord2VecSkipGram = word2vec.skipGramDocumentRepresentation()
-listWord2VecCBOW = word2vec.continuousBagOfWordsDocumentRepresentation()
+# listDoc2vecDBOW = doc2vec.paragraphVectorDistributedBagOfWords()
+# listDoc2vecConcat = doc2vec.concatBothParagraphVectors()
 
-tfidf = TermFrequencyInverseDocumentFrequency()
-listTfIdf = tfidf.createVectors(newlistNews)
+# word2vec = DocumentRepresentationWord2Vec(newlistNews)
+# listWord2VecSkipGram = word2vec.skipGramDocumentRepresentation()
+# listWord2VecCBOW = word2vec.continuousBagOfWordsDocumentRepresentation()
+
+# tfidf = TermFrequencyInverseDocumentFrequency()
+# listTfIdf = tfidf.createVectors(newlistNews)
 
 # print("########################################### DOC2VEC - DM ###########################################")
 # print(len(listDoc2vecDM[0]))
@@ -50,7 +60,21 @@ listTfIdf = tfidf.createVectors(newlistNews)
 # print(len(listTfIdf[0]))
 # print(listTfIdf)
 
+npList = []
+for document in listDoc2vecDM:
+    npList.append(np.array(document))
+npListLabel = []
+for label in listLabel:
+    npListLabel.append(np.array(label))
 
+npList = np.array(npList)
+npList = np.expand_dims(npList, axis=2)
+npListLabel = np.array(npListLabel)
 
-
-
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(100,1)),
+    keras.layers.Dense(100, activation='relu'),
+    keras.layers.Dense(1)
+])
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+model.fit(npList, npListLabel, epochs=100)
